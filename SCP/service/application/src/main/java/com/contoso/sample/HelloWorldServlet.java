99867cb7e3680c01766497666fc1e8ff36616476
raepple <martin@raepple.net>
2022-06-08 09:00:03 +0200

    Fixed class cast exception

--- SCP/service/application/src/main/java/com/contoso/sample/HelloWorldServlet.java
+++ SCP/service/application/src/main/java/com/contoso/sample/HelloWorldServlet.java
@@ -23,31 +23,34 @@ import org.slf4j.LoggerFactory;
 
 import io.vavr.control.Try;
 
-
 @WebServlet("/hello")
 @ServletSecurity(@HttpConstraint(rolesAllowed = { "Display" }))
-public class HelloWorldServlet extends HttpServlet
-{
+public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(HelloWorldServlet.class);
 
     @Override
-    protected void doGet( final HttpServletRequest request, final HttpServletResponse response )
-        throws IOException
-    {
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException {
         logger.info("I am running!");
-        Try<Principal> currentUser = new DefaultPrincipalFacade().tryGetCurrentPrincipal();    
+        Try<Principal> currentUser = new DefaultPrincipalFacade().tryGetCurrentPrincipal();
         if (currentUser.isSuccess()) {
-            DefaultPrincipal defaultCurrentUser = (DefaultPrincipal)currentUser.get();            
+            DefaultPrincipal defaultCurrentUser = (DefaultPrincipal) currentUser.get();
             response.getWriter().println("Hello " + defaultCurrentUser.getPrincipalId());
             Iterator<PrincipalAttribute> i = defaultCurrentUser.getAttributes().values().iterator();
             while (i.hasNext()) {
-                CollectionPrincipalAttribute<StringPrincipalAttribute> attribute = (CollectionPrincipalAttribute<StringPrincipalAttribute>) i.next();                
-                response.getWriter().println("Attribute: " + attribute.getName() + ", Value: " + attribute.getValues().iterator().next());
+                CollectionPrincipalAttribute<PrincipalAttribute> attribute = (CollectionPrincipalAttribute<PrincipalAttribute>) i
+                        .next();
+                if (attribute.getValues().size() > 0) {
+                    response.getWriter()
+                            .println("Attribute: " + attribute.getName() + ", Value: " + attribute.getValues());
+                } else {
+                    logger.info("Attribute {} is empty", attribute.getName());
+                }
             }
-            Iterator<Authorization> j = ((DefaultPrincipal)currentUser.get()).getAuthorizations().iterator();
+            Iterator<Authorization> j = ((DefaultPrincipal) currentUser.get()).getAuthorizations().iterator();
             while (j.hasNext()) {
-                Authorization scope = (Authorization)j.next();
+                Authorization scope = (Authorization) j.next();
                 response.getWriter().println("Scope: " + scope.getName());
             }
         } else {
